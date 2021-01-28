da7a5a0291f142febddc47b215372586de6b7ce9
Iansoft <iansoftachannels@gmail.com>
2021-01-28 16:09:20 +0300

    training module viewing, listing with datatables and links to images

--- frontend/views/training/view.php
+++ frontend/views/training/view.php
@@ -26,22 +26,109 @@ $this->params['breadcrumbs'][] = $this->title;
         ]) ?>
     </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'organizer',
-            'date',
-            'topic',
-            'venue',
-            'facilitators:ntext',
-            'no_of_participants:ntext',
-            'participants_scan',
-            'created_by',
-            'updated_by',
-            'created_at',
-            'updated_at',
-        ],
-    ]) ?>
+    <div class="card">
+        <div class="card-body">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                  <h3 class="box-title" data-speechify-sentence="">About This Training</h3>
+                </div>
+            <!-- /.box-header -->
+                <div class="box-body row" data-read-aloud-multi-block="true">
+                    <div class="col-md-6">
+                        <strong data-speechify-sentence=""><i class="fas fa-address-card"></i> Organizer</strong>
+
+                        <p class="text-muted" data-speechify-sentence="">
+                            <?= $model->organizer ?>
+                        </p>
+                        <hr>
+                    </div>
+                  
+                    <div class="col-md-6">
+                        <strong data-speechify-sentence=""><i class="fas fa-venus-mars"></i> Date</strong>
+
+                        <p class="text-muted" data-speechify-sentence="">
+                            <?=  date("l M j, Y",$model->date) ?>
+                        </p>
+                        <hr>
+                    </div> 
+                    <div class="col-md-6">
+                        <strong data-speechify-sentence=""><i class="far fa-address-book"></i> Topic</strong>
+
+                        <p class="text-muted" data-speechify-sentence="">
+                            <?= $model->topic ?>
+                        </p>
+                        <hr>
+                    </div>
+                    <div class="col-md-6">
+                        <strong data-speechify-sentence=""> Venue</strong>
+
+                        <p class="text-muted" data-speechify-sentence="">
+                            <?= $model->venue ?>
+                        </p>
+                        <hr>
+                    </div> 
+                    <div class="col-md-6">
+                        <strong data-speechify-sentence=""> Facilitators</strong>
+
+                        <p class="text-muted" data-speechify-sentence="">
+                            <?= $model->facilitators ?>
+                        </p>
+                        <hr>
+                    </div>  
+                    <div class="col-md-6">
+                        <strong data-speechify-sentence="">  No. of Participants</strong>
+
+                        <p class="text-muted" data-speechify-sentence="">
+                            <?= $model->no_of_participants ?>
+                        </p>
+                        <hr>
+                    </div>  
+                    
+                    <div class="col-md-6">
+                      <strong data-speechify-sentence=""><i class="fas fa-calendar-day"></i> Created On</strong>
+
+                      <p data-speechify-sentence=""><?= date("H:ia l M j, Y",$model->created_at)?></p>
+                    </div>
+
+                    <div class="col-md-12">
+                      <strong data-speechify-sentence=""><i class="fas fa-calendar-day"></i> Participants</strong>
+
+                      <p data-speechify-sentence=""><img src="/uploads/training/<?= $model->participants_scan ?>" width="300px"></p>
+                    </div>
+                </div>
+            <!-- /.box-body -->
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <h4 class="card-title">Files</h4>
+        </div>
+        <div class="card-body">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                  <h3 class="box-title" data-speechify-sentence="">Uploads for this Training</h3>
+                </div>
+            <!-- /.box-header -->
+                <div class="box-body row" data-read-aloud-multi-block="true">
+                    <?php
+                    foreach ($model->trainingUploads as $file) {
+                    ?>
+                        <div class="col-md-6">
+                            <strong data-speechify-sentence=""><i class="fas fa-file"></i> </strong>
+
+                            <p class="text-muted" data-speechify-sentence="">
+                                <?= Html::a('Preview Document: '.$file->filename, ['/uploads/multiple/training/'.$file->filename], ['class' => 'label label-primary', 'target' => '_blank', 'title'=> $file->filename]) ?>
+                            </p>
+                            <hr>
+                        </div>
+                    <?php 
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>
