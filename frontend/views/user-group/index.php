49b318b643bb01f2143d1747efeeaaa0f299af75
Iansoft <iansoftachannels@gmail.com>
2021-03-05 13:55:34 +0300

    permissions extention on the server

--- frontend/views/user-group/index.php
+++ frontend/views/user-group/index.php
@@ -15,7 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create User Group', ['create'], ['class' => 'btn btn-success']) ?>
+        <?php Html::a('Create User Group', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
@@ -34,34 +34,7 @@ $this->params['breadcrumbs'][] = $this->title;
             //'created_by',
             //'updated_by',
 
-            ['class' => 'yii\grid\ActionColumn','buttons' => [
-
-                'view' => function( $url )
-                {
-                    return Html::a('<i class="fa fa-eye"></i>', $url,[]);
-                },
-
-                'update' => function( $url )
-                {
-                    return Html::a('<i class="fa fa-edit"></i>', $url,[]);
-                },
-
-                'delete' => function( $url )
-                {
-                    return Html::a('<i class="fa fa-trash"></i>', $url,[
-
-                        'data' => [
-                            'confirm' => 'Are you sure you wanna delete this record ?',
-                            'method' => 'POST',
-                            'params' => [
-                                '_csrf-frontend' => Yii::$app->request->csrfToken
-                            ]
-
-                        ]
-                    ]);
-                }
-
-            ],],
+            ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
 
