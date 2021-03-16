c9f34ac9373d5dc9fb49e1b79cf3a9faa436f782
Iansoft <iansoftachannels@gmail.com>
2021-03-16 09:20:22 +0300

    test include credentials

--- frontend/web/sw.js
+++ frontend/web/sw.js
@@ -18,6 +18,19 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   // Perform some task
   console.log('[Service Worker] Activating Service Worker ....', event);
+  var cacheAllowlist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
