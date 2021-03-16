e88f8a2c2d7201c9bca2e6f3bd4943a7c5278864
Iansoft <iansoftachannels@gmail.com>
2021-03-16 11:50:23 +0300

    activate service worker

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -523,7 +523,17 @@ $absoluteUrl = \yii\helpers\Url::home(true);
 <?php //$this->registerJsFile(""); ?>
 <!-- END OF ADD SWEET ALERT -->
 <script>
-        
+        if('serviceWorker' in navigator){
+            window.addEventListener('load', function() {
+                navigator.serviceWorker.register('/sw.js').then(function(registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                }, function(err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+                });
+            });
+        }
     </script>
 </body>
 </html>
