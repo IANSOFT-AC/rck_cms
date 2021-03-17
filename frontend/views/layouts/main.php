ef0ebfa996e6e8d783cb3acd4467d600ab6a9edd
Iansoft <iansoftachannels@gmail.com>
2021-03-17 09:16:20 +0300

    request for push permission from user to notify them when internet comes and goes

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -526,12 +526,15 @@ $absoluteUrl = \yii\helpers\Url::home(true);
 <script>
         if('serviceWorker' in navigator){
             window.addEventListener('load', function() {
-                navigator.serviceWorker.register('/sw.js').then(function(registration) {
-                // Registration was successful
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                navigator.serviceWorker.register('/sw.js')
+                .then(registration => navigator.serviceWorker.ready)
+                .then(function(registration) {
+                    // Registration was successful
+                    Notification.requestPermission();
+                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
                 }, function(err) {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
+                    // registration failed :(
+                    console.log('ServiceWorker registration failed: ', err);
                 });
             });
         }
