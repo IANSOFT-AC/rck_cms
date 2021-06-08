f4638f9ee4295cea7c211dfe431eea22d78d4a42
mwanginjamb <francnjamb@gmail.com>
2021-06-08 18:53:31 +0300

    movement to live

---
+++ common/config/main.php
@@ -0,0 +1,42 @@
+<?php
+return [
+    'name' => 'RCK CMS',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'recruitment' => [
+            'class' => 'common\Library\Recruitment'
+        ],
+        'assetManager' => [
+            'bundles' => [
+                'yii\web\JqueryAsset' => [
+                    'sourcePath' => null,
+                    'js' => ['/plugins/jquery/jquery.js'],
+                ]
+            ]
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                [
+                    'class' => \yii\rest\UrlRule::class,
+                    'controller' => ['api/client']
+                ]
+            ],
+        ],
+        
+    ],
+    // Declare modules outside components arrray
+    /*'modules' => [
+        'api' => [
+            'class' => \common\modules\api\Module::class
+        ]
+    ]*/
+];
