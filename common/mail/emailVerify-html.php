cb6168f2f10fecb8484a53a25d7d443e291d53af
mwanginjamb <francnjamb@gmail.com>
2021-06-15 23:48:11 +0300

    Fix: signup email sends user a password reset link

--- common/mail/emailVerify-html.php
+++ common/mail/emailVerify-html.php
@@ -4,12 +4,13 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $user common\models\User */
 
-$verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['site/verify-email', 'token' => $user->verification_token]);
+// $verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['site/verify-email', 'token' => $user->verification_token]);
+$verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['site/request-password-reset']);
 ?>
 <div class="verify-email">
     <p>Hello <?= Html::encode($user->username) ?>,</p>
 
-    <p>Follow the link below to verify your email:</p>
+    <p>Follow the link below to reset your password and gain access to the CMS :</p>
 
     <p><?= Html::a(Html::encode($verifyLink), $verifyLink) ?></p>
 </div>
