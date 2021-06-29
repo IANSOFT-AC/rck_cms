e3490b8a61cb0c70ae28f35bd4ee6e2df5239b4d
IANSOFT-AC <77881487+IANSOFT-AC@users.noreply.github.com>
2021-06-29 11:03:07 +0300

    Create main-local.php

---
+++ common/config/main-local.php
@@ -0,0 +1,36 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname='.env('DB_NAME'),
+            'username' => 'root',
+            'password' => env('DB_PASSWORD'),
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'mail.softeboard.com',
+                'username' => 'rck.support@softeboard.com',
+                'password' => '@Rck2021#*',
+                'port' => '587',
+                /*'encryption' => 'tls',
+                'streamOptions' => [ 'ssl' =>
+                    [
+                        'allow_self_signed' => true,
+                        'verify_peer' => true,
+                        'verify_peer_name' => true,
+                    ],
+                ],*/
+            ],
+        ],
+    ],
+];
